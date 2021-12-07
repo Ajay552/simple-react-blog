35f69557abac7c06233b5d49caf1e42f0d6e7edf
ajay_S <sajayyy01234@gmail.com>
2021-12-07 22:09:37 +0530

    added initial Navbar,Home and style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />  {/* can also be written as <Navbar></Navbar> insted of self closing tag */}
      <div className="content">
-       <h1>App Component</h1>
+       <Home/>
      </div>
     </div>
   );
