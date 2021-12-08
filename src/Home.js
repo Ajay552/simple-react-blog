5449622c913708901767db82e315e63eca6637f2
ajay_S <sajayyy01234@gmail.com>
2021-12-08 19:02:02 +0530

    added initial useState hook

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 const Home = () => {
+
+    // useState hooks
+    const [name,setName] = useState("Ajay");
+    const [age, setAge] = useState("20");
+    const handleClick = () => {
+        setName("Nova");
+        setAge(21);
+    }
+
     return (
         <div className="home">
-            <h1>Hello World</h1>
+            <h1>Homepage</h1>
+            <p>{ name } is { age } years old</p>
+            <button onClick={handleClick}>Click me</button>
         </div>
     );
 }
