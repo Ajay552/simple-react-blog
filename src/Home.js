ae32410fa25f7871715c9d678241f0ad4fe84995
ajay_S <sajayyy01234@gmail.com>
2021-12-08 21:00:52 +0530

    installed json-server and used fetch api

--- src/Home.js
+++ src/Home.js
@@ -2,11 +2,7 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
+    const [blogs, setBlogs] = useState(null);
 
     const [name, setName] = useState("Nova");
 
@@ -16,15 +12,19 @@ const Home = () => {
     }
 
     useEffect(() => {
-        console.log("use effect ran");
-        console.log(name);
-    }, [name]);
+        fetch(" http://localhost:8000/blogs")
+        .then(res => {
+            return res.json();
+        })
+        .then((data) => {
+            console.log(data);
+            setBlogs(data);
+        })
+    }, []);
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
-            <button onClick={() => setName("Ajay")}>change name</button>
-            <p>{ name }</p>
+            {blogs &&<BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>}
         </div>
     );
 }
