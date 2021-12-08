dff40cb51c1024dd46c2f75364b50bc03b9fb919
ajay_S <sajayyy01234@gmail.com>
2021-12-08 21:59:58 +0530

    adder error handling

--- src/Home.js
+++ src/Home.js
@@ -3,28 +3,35 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
-
-    const [name, setName] = useState("Nova");
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         fetch(" http://localhost:8000/blogs")
         .then(res => {
+            // console.log(res);
+            if(!res.ok){
+                throw Error("could not fetch the data");
+            }
             return res.json();
         })
         .then((data) => {
             console.log(data);
             setBlogs(data);
+            setIsPending(false);
+            setError(null);
+        })
+        .catch(err => {
+            setError(err.message);
+            setIsPending(false);
         })
     }, []);
 
     return (
         <div className="home">
-            {blogs &&<BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>}
+            { error && <div>{ error }</div> } 
+            { isPending && <div>Loading.....</div> }
+            {blogs &&<BlogList blogs={blogs} title="All Blogs"/>}
         </div>
     );
 }
