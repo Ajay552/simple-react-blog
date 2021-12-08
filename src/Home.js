59fb459643cb2dfac2a7160231382899ec51426f
ajay_S <sajayyy01234@gmail.com>
2021-12-08 20:08:17 +0530

    added initial props

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,16 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-
-    // useState hooks
-    const [name,setName] = useState("Ajay");
-    const [age, setAge] = useState("20");
-    const handleClick = () => {
-        setName("Nova");
-        setAge(21);
-    }
-
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
     return (
         <div className="home">
-            <h1>Homepage</h1>
-            <p>{ name } is { age } years old</p>
-            <button onClick={handleClick}>Click me</button>
+            <BlogList blogs={blogs} title="All Blogs"/>
+            <BlogList blogs={blogs.filter((blog) => blog.author === "mario" )} title="mario's blogs"/>
         </div>
     );
 }
